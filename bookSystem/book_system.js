53d772c2bd2ae5323feac696dea026e00ad44d72
sam <samtrott94@gmail.com>
2026-02-15 13:45:00 -0500

    speed typing

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -26,12 +26,23 @@ let books = [];
                 <p><strong>Author Name:</strong> ${book.authorName}</p>
                 <p><strong>Book Description:</strong> ${book.bookDescription}</p>
                 <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
-                <button onclick="editbook(${index})">Edit</button>`
+                <button onclick="editbook(${index})">Edit</button>
+                <button onclick="deletebook(${index})">Delete</button>`
             );
             document.getElementById('books').innerHTML = booksDiv.join('');
         }
 
         function editbook(index) {
+            const book = books[index];
+            document.getElementById('bookName').value = book.name;
+            document.getElementById('authorName').value = book.authorName;
+            document.getElementById('bookDescription').value = book.bookDescription;
+            document.getElementById('pagesNumber').value = book.pagesNumber;
+            books.splice(index, 1); // Remove old entry
+            showbooks(); // Refresh list
+          }
+
+          function deletebook(index) {
             const book = books[index];
             document.getElementById('bookName').value = book.name;
             document.getElementById('authorName').value = book.authorName;
